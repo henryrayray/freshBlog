e9f5cf9c2d20eb6fa52b76250fd9b97e8cb5df42
Howon Song <hs459@cornell.edu>
2015-01-25 20:50:00 -0500

    blog structure is done

--- blog/blog.js
+++ blog/blog.js
@@ -1,4 +1,13 @@
 $(function() {
-  // var posts = {% site.posts %};
-  // debugger
+  var loc = window.location;
+  var category = loc.pathname.split("/")[1];
+  var possible_categories = ["general", "ideas", "development"];
+  for (var i=0; i<possible_categories.length; i++) {
+    var p_c = possible_categories[i];
+    if (category == p_c) {
+      $('li.'+p_c).addClass("active");
+    }
+  }
 })
+
+// used in /_layouts/post.html
